c0dcd71b4f4cf57465cf756af9c6b9643041e06f
Wolkenfarmer <jomaxw2@aol.com>
2024-01-11 20:50:41 +0100

    #8 add and configure linter

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+    extends: [
+        // add more generic rulesets here, such as:
+        'eslint:recommended',
+        'plugin:vue/vue3-recommended',
+    ],
+    rules: {
+        // override/add rules settings here, such as:
+        // 'vue/no-unused-vars': 'error'
+        'camelcase': 'warn',
+        'max-len': ['error', {
+            'code': 150,
+            'tabWidth': 4,
+        }],
+
+        'vue/camelcase': 'warn',
+        'vue/max-len': ['error', {
+            'code': 150,
+            'template': 150,
+            'tabWidth': 4,
+        }],
+    }
+}
