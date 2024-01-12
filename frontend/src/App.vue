4fa2b6764547cb16ac7087bb8cc630bacfb95014
JoshuaRiewesell <40123349+JoshuaRiewesell@users.noreply.github.com>
2024-01-12 10:25:54 +0100

    #6: implement basic project view structure (#7)
    * #6: Modules as Dynamic Components, dev-bar
    
    * #6: refactor folder setup
    
    * #6: add component structure explanation to frontend Readme
    
    * #6: adjust default module viewing experience
    
    ---------
    
    Co-authored-by: Wolkenfarmer <jomaxw2@aol.com>

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,47 +1,25 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue'
+import ModuleLogin from '@/components/ModuleLogin.vue'
+import ModuleTrainer from '@/components/ModuleTrainer.vue'
+import ModulePatient from '@/components/ModulePatient.vue'
+
+const modules = {
+  ModuleLogin,
+  ModuleTrainer,
+  ModulePatient
+}
+
+const currentModule = ref('ModulePatient')
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/icon.png" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <component :is="modules[currentModule]"></component>
   </main>
+  <div id="dev-bar">
+    <button @click="currentModule='ModuleLogin'">Login</button>
+    <button @click="currentModule='ModuleTrainer'">Trainer</button>
+    <button @click="currentModule='ModulePatient'">Patient</button>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
