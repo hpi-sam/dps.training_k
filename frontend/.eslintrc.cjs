d80a0cd22ad79b9a47de706f927537ced164c434
Wolkenfarmer <jomaxw2@aol.com>
2024-01-13 16:04:31 +0100

    #8 add and configure linter (#9)
    * #8: add and configure linter
    
    * #8: change singleline attributes behaviour

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+    extends: [
+        // add more generic rulesets here, such as:
+        'eslint:recommended',
+        'plugin:vue/vue3-recommended',
+    ],
+    rules: {
+        // override/add rules settings here, such as:
+        // 'vue/no-unused-vars': 'error'
+        'camelcase': 'warn',
+        'max-len': ['error', {
+            'code': 150,
+            'tabWidth': 4,
+        }],
+
+        'vue/camelcase': 'warn',
+        'vue/max-len': ['error', {
+            'code': 150,
+            'template': 150,
+            'tabWidth': 4,
+        }],
+
+        "vue/max-attributes-per-line": ["error", {
+            "singleline": {
+                "max": 4
+            },
+            "multiline": {
+                "max": 1
+            }
+        }]
+    }
+}
