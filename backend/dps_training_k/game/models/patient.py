7d113e36370c607a4e4393c7b135b03f37140134
Toni000 <tonipor27@gmail.com>
2024-01-30 20:20:24 +0100

    #23: add hardcoded patient

--- backend/dps_training_k/game/models/patient.py
+++ backend/dps_training_k/game/models/patient.py
@@ -3,10 +3,10 @@ from django.db import models
 
 class Patient(models.Model):
     name = models.CharField(
-        max_length=100
+        max_length=100, default="Max Mustermann"
     )  # technically patientData but kept here for simplicity for now
     # patientID = models.ForeignKey()  # currently called "SensenID"
-    # exerciseID = models.ForeignKey()
+    exercise = models.ForeignKey("Exercise", on_delete=models.CASCADE)
     # stateID = models.ForeignKey()
     # measureID = models.ManyToManyField()
     patientCode = models.IntegerField(
