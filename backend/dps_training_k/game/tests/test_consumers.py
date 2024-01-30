dba06ecab5f10012aae44b310019d78d03b8ea95
Toni Poredda <66776809+Toni000@users.noreply.github.com>
2024-01-30 20:18:37 +0100

    #47: add test passthrough event and corresponding test (#49)

--- backend/dps_training_k/game/tests/test_consumers.py
+++ backend/dps_training_k/game/tests/test_consumers.py
@@ -61,6 +61,22 @@ class TrainerConsumerTestCase(TransactionTestCase):
         # Close the connection
         await communicator.disconnect()
 
+    async def test_trainer_consumer_test_passthrough_request(self):
+        path = "/ws/trainer/"
+        communicator = WebsocketCommunicator(application, path)
+        connected, _ = await communicator.connect()
+        self.assertTrue(connected)
+
+        # Send an "example" request type message to the server
+        await communicator.send_json_to({"type": "test-passthrough"})
+
+        # Receive and test the response from the server
+        response = await communicator.receive_json_from()
+        self.assertEqual(response, {"type": "test-passthrough", "message": "received test event"})
+
+        # Close the connection
+        await communicator.disconnect()
+
 
 class PatientConsumerTestCase(TransactionTestCase):
     async def test_trainer_consumer_example_request(self):
