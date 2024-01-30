5d09832c585230c770e53c2edb4c253fffac5600
Toni Poredda <66776809+Toni000@users.noreply.github.com>
2024-01-30 20:01:45 +0100

    27 m00 enable creating an exercise (#44)
    * #27: add missing requirement in requirements.txt
    
    * #27: fix conflicting migrations
    
    * #27: add create exercise event handling
    
    * #27: change invitation code generation to store already taken codes by itself
    
    * #27: fix tests

--- backend/dps_training_k/game/tests/test_consumers.py
+++ backend/dps_training_k/game/tests/test_consumers.py
@@ -1,9 +1,10 @@
 from channels.testing import WebsocketCommunicator
-from django.test import TestCase
+from django.test import TransactionTestCase
 from dps_training_k.asgi import application
 
 
-class TrainerConsumerTestCase(TestCase):
+class TrainerConsumerTestCase(TransactionTestCase):
+    maxDiff = None
     async def test_trainer_consumer_example_request(self):
         path = "/ws/trainer/"
         communicator = WebsocketCommunicator(application, path)
@@ -19,9 +20,49 @@ class TrainerConsumerTestCase(TestCase):
 
         # Close the connection
         await communicator.disconnect()
+    
+    async def test_trainer_handle_create_exercise(self):
+        path = "/ws/trainer/"
+        communicator = WebsocketCommunicator(application, path)
+        connected, _ = await communicator.connect()
+        self.assertTrue(connected)
+
+        # Send an "example" request type message to the server
+        await communicator.send_json_to({"type": "trainer-exercise-create"})
+
+        # Receive and test the response from the server
+        response = await communicator.receive_json_from()
+        content = {
+            "exerciseCode": "123456",
+            "areas": [
+                {
+                    "name": "ZNA",
+                    "patients": [
+                        {
+                            "name": "Max Mustermann",
+                            "patientCode": 123456,
+                            "patientId": 5,
+                            "patientDatabaseId": 3,
+                        }
+                    ],
+                    "personnel": [
+                        {
+                            "name": "Hanna Schulz",
+                            "role": "Arzt",
+                            "personnelDatabaseId": 6,
+                        }
+                    ],
+                    "devices": [{"name": "EKG", "deviceDatabaseId": 15}],
+                }
+            ],
+        }
+        self.assertEqual(response, {"type": "trainer-exercise-create", "exercise": content})
+
+        # Close the connection
+        await communicator.disconnect()
 
 
-class PatientConsumerTestCase(TestCase):
+class PatientConsumerTestCase(TransactionTestCase):
     async def test_trainer_consumer_example_request(self):
         path = "/ws/patient/"
         communicator = WebsocketCommunicator(application, path)
--- backend/dps_training_k/helpers/invitation_logic.py
+++ backend/dps_training_k/helpers/invitation_logic.py
@@ -6,8 +6,9 @@ from Levenshtein import distance as lev
 class LevenshteinCode:
     def __init__(self, code_length):
         self.code_length = code_length
+        self.codes_taken = []
 
-    def get_invitation_code(self, codes_taken):
+    def get_invitation_code(self):
         """
         Generates an unique alphabetic invitation_code of length self.code_length,
         that is also distinctive from other non-finished excercises (levenshtein distance >= 3).
@@ -17,9 +18,10 @@ class LevenshteinCode:
         letters = string.ascii_lowercase
         while not new_code:
             new_code = "".join(random.choice(letters) for _ in range(self.code_length))
-            for code_taken in codes_taken:
+            for code_taken in self.codes_taken:
                 if (
                     lev(str(code_taken), str(new_code)) < 3
                 ):  # should be different at minimum 3 places
                     new_code = None
+        self.codes_taken.append(new_code)
         return new_code
