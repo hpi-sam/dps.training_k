dacd07e7e6a3c199af58d65f31335d9869d9e5e6
Wolkenfarmer <jomaxw2@aol.com>
2024-02-26 18:48:59 +0100

    #63 frontend: move svg paths to separate file

--- frontend/src/components/widgets/LoginTrainer.vue
+++ frontend/src/components/widgets/LoginTrainer.vue
@@ -2,6 +2,7 @@
 	import {ref} from 'vue'
 	import {useTrainerStore} from '@/stores/Trainer';
 	import {Modules, setModule, showErrorToast} from "@/App.vue";
+	import {svg} from "@/assets/Svg";
 
 	const usernameInput = ref("")
 	const passwordInput = ref("")
@@ -53,8 +54,7 @@
 			<input v-model="passwordInput" type="password" placeholder="Passwort">
 			<button @click="submit()">
 				<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
-					<!-- eslint-disable-next-line max-len, vue/max-len -->
-					<path d="M480-120v-80h280v-560H480v-80h280q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H480Zm-80-160-55-58 102-102H120v-80h327L345-622l55-58 200 200-200 200Z" />
+					<path :d="svg.loginIcon" />
 				</svg>
 			</button>
 		</div>
