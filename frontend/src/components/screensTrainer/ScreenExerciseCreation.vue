8705c1fc40f3088664daf010ecffffb112b80f25
Wolkenfarmer <jomaxw2@aol.com>
2024-02-29 17:46:06 +0100

    #21 frontend: trainer: add top bar to log screen

--- frontend/src/components/screensTrainer/ScreenExerciseCreation.vue
+++ frontend/src/components/screensTrainer/ScreenExerciseCreation.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
-	import {useTrainerStore} from '@/stores/Trainer'
 	import {useExerciseStore} from '@/stores/Exercise'
 	import socketTrainer from "@/sockets/SocketTrainer"
 	import ButtonMainAction from "@/components/widgets/ButtonMainAction.vue"
+	import TopBarTrainer from "@/components/widgets/TopBarTrainer.vue"
 
-	const trainerStore = useTrainerStore()
 	const exerciseStore = useExerciseStore()
 
 	function exerciseStart() {
@@ -13,44 +12,10 @@
 </script>
 
 <template>
-	<nav>
-		<div id="nav-trainer">
-			{{ trainerStore.username }}
-		</div>
-		<div id="nav-exercise-code">
-			{{ exerciseStore.exerciseId }}
-		</div>
-	</nav>
+	<TopBarTrainer />
 	<div>Geladenes Übungsobjekt: <br>{{ exerciseStore }}</div>
 	<ButtonMainAction
 		:button-text="'Übung starten'"
 		@on-pressed="exerciseStart"
 	/>
 </template>
-
-<style scoped>
-	nav {
-		width: 100%;
-		height: 60px;
-		display: flex;
-		float: left;
-	}
-
-	#nav-trainer, #nav-exercise-code {
-		width: 50%;
-		height: 100%;
-		border-bottom: 8px solid black;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		font-size: 1.5em;
-	}
-
-	#nav-trainer {
-		border-right: 4px solid black;
-	}
-
-	#nav-exercise-code {
-		border-left: 4px solid black;
-	}
-</style>
--- frontend/src/components/screensTrainer/ScreenLog.vue
+++ frontend/src/components/screensTrainer/ScreenLog.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+	import TopBarTrainer from "@/components/widgets/TopBarTrainer.vue"
+</script>
+
 <template>
+	<TopBarTrainer />
 	Log Screen
 </template>
