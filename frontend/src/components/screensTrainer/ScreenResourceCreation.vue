dfc0fbd67126ba39561ffc0f3fc1ce5b53527f02
JoshuaRiewesell <40123349+JoshuaRiewesell@users.noreply.github.com>
2024-03-11 15:47:54 +0100

    #78 frontend: add navbar for ScreenResourceCreation

--- frontend/src/components/screensTrainer/ScreenResourceCreation.vue
+++ frontend/src/components/screensTrainer/ScreenResourceCreation.vue
@@ -1,20 +1,100 @@
 <script setup lang="ts">
+	import PagePatients from './pagesResourceCreation/PagePatients.vue'
+	import PagePersonnel from './pagesResourceCreation/PagePersonnel.vue'
+	import PageMaterial from './pagesResourceCreation/PageMaterial.vue'
+	import {computed, ref} from 'vue'
 	import {useExerciseStore} from '@/stores/Exercise'
+
+	enum Pages {
+		PATIENTS = "PagePatients",
+		PERSONNEL = "PagePersonnel",
+		MATERIAL = "PageMaterial",
+	}
+
+	const currentPage = ref(Pages.PATIENTS)
+	const currentPageComponent = computed(() => getPageComponent(currentPage.value))
+
+	const getPageComponent = (page: Pages) => {
+		switch (page) {
+			case Pages.PATIENTS:
+				return PagePatients
+			case Pages.PERSONNEL:
+				return PagePersonnel
+			case Pages.MATERIAL:
+				return PageMaterial
+		}
+	}
+
+	const setPage = (newPage: Pages) => {
+		currentPage.value = newPage
+	}
 
 	const exerciseStore = useExerciseStore()
 
 	const currentAreaData = computed(() => exerciseStore.getArea(currentArea.value))
 </script>
+
 <script lang="ts">
-	import {computed, ref} from 'vue'
-	
 	const currentArea = ref("")
 
 	export const setArea = (newArea: string) => {
 		currentArea.value = newArea
 	}
 </script>
+
 <template>
 	<h1>ResourceCreation</h1>
 	{{ currentAreaData }}
+	<component :is="currentPageComponent" />
+	<nav>
+		<button id="nav-patients" :class="{ 'selected': currentPage === Pages.PATIENTS }" @click="setPage(Pages.PATIENTS)">
+			Patienten
+		</button>
+		<button id="nav-personnel" :class="{ 'selected': currentPage === Pages.PERSONNEL }" @click="setPage(Pages.PERSONNEL)">
+			Personal
+		</button>
+		<button id="nav-material" :class="{ 'selected': currentPage === Pages.MATERIAL }" @click="setPage(Pages.MATERIAL)">
+			Material
+		</button>
+	</nav>
 </template>
+
+<style scoped>
+	nav {
+		width: 100%;
+		height: 60px;
+		bottom: 0;
+		position: absolute;
+		display: flex;
+		float: left;
+	}
+
+	button {
+		width: calc(100% / 3);
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		font-size: 1.5em;
+		background-color: lightgray;
+		border: none;
+		border-top: 8px solid black;
+	}
+
+	button.selected {
+		background-color: white;
+		font-weight: bold;
+	}
+
+	#nav-patients {
+		border-right: 4px solid black;
+	}
+
+	#nav-personnel {
+		border-left: 4px solid black;
+		border-right: 4px solid black;
+	}
+
+	#nav-material {
+		border-left: 4px solid black;
+	}
+</style>
