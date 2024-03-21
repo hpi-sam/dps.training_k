87e13228564b7a90337ab541ba25cd6e3a706738
claasga <claas.gade@gmail.com>
2024-03-21 15:15:38 +0100

    #29 backend: delete broken test

--- backend/dps_training_k/game/tests/test_consumers.py
+++ backend/dps_training_k/game/tests/test_consumers.py
@@ -61,26 +61,3 @@ class TrainerConsumerTestCase(TransactionTestCase):
 
         # Close the connection
         await communicator.disconnect()
-
-
-class PatientConsumerTestCase(TransactionTestCase):
-    async def test_trainer_consumer_example_request(self):
-        path = "/ws/patient/"
-        communicator = WebsocketCommunicator(application, path)
-        connected, _ = await communicator.connect()
-        self.assertTrue(connected)
-
-        # Send an "example" request type message to the server
-        await communicator.send_json_to(
-            {"type": "example", "exercise_code": "123", "patient_code": "123456"}
-        )
-
-        # Receive and test the response from the server
-        response = await communicator.receive_json_from()
-        self.assertEqual(
-            response,
-            {"messageType": "test-passthrough", "message": "received test event"},
-        )
-
-        # Close the connection
-        await communicator.disconnect()
