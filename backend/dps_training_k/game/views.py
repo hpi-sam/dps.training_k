8be56d035fbefe4b56482ad444ea6774f6b14a2b
Toni000 <tonipor27@gmail.com>
2024-03-25 14:33:38 +0100

    #109 backend: add corsheaders as well as default user for testing

--- backend/dps_training_k/game/views.py
+++ backend/dps_training_k/game/views.py
@@ -3,17 +3,21 @@ from rest_framework import status
 from rest_framework.response import Response
 from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
-
+from game.models import User
 
 class PatientAccessView(APIView):
     def post(self, request, *args, **kwargs):
-        if not (request.data.get("exerciseCode") and request.data.get("patientCode")):
+        user, created = User.objects.get_or_create(username='123')  # Ensure the username is a string
+        if created:
+            user.set_password("123")  # Properly hash the password
+            user.save()
+        if not (request.data.get("exerciseId") and request.data.get("patientId")):
             return Response(
                 status=status.HTTP_400_BAD_REQUEST,
                 data="Some required fields are missing",
             )
-        exercise_code = request.data.get("exerciseCode")
-        patient_code = request.data.get("patientCode")
+        exercise_code = request.data.get("exerciseId")
+        patient_code = request.data.get("patientId")
         user = authenticate(username=exercise_code, password=patient_code)
         if user:
             token, created = Token.objects.get_or_create(user=user)
