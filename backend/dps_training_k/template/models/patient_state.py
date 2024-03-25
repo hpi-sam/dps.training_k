5108ba3909c4ae911d7b8d8dbeec80a884283219
claasga <claas.gade@gmail.com>
2024-03-25 16:44:20 +0100

    #30 backend: switch files to directories, switch PatientState into template

--- backend/dps_training_k/template/models/patient_state.py
+++ backend/dps_training_k/template/models/patient_state.py
@@ -1,12 +1,13 @@
 from django.db import models
-from .patient import Patient
 
 
 class PatientState(models.Model):
     stateID = models.IntegerField(
         help_text="state number as it is used in original data set"
     )
-    patientID = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    patientID = models.ForeignKey(
+        "Patient", on_delete=models.CASCADE, related_name="state"
+    )
     data = models.JSONField(help_text="data for patient in current phase")
     phase = models.IntegerField(help_text="current phase, e.g. 3")
 
