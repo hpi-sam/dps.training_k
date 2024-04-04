290487f38d0dd4f6da45cb72cda1437a831f4128
Wolkenfarmer <jomaxw2@aol.com>
2024-04-04 18:18:59 +0200

    #123: fix patient login error messages

--- backend/dps_training_k/game/views.py
+++ backend/dps_training_k/game/views.py
@@ -1,8 +1,9 @@
-from rest_framework.views import APIView
-from rest_framework import status
-from rest_framework.response import Response
 from django.contrib.auth import authenticate
+from rest_framework import status
 from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from game.models import User
 
 
@@ -22,11 +23,13 @@ class PatientAccessView(APIView):
             )
         exercise_id = str(request.data.get("exerciseId"))
         patient_id = str(request.data.get("patientId"))
+
         user = authenticate(username=exercise_id, password=patient_id)
+
         if user:
             token, created = Token.objects.get_or_create(user=user)
             return Response({"token": token.key}, status=status.HTTP_200_OK)
         else:
             return Response(
-                {"error": "Wrong Credentials"}, status=status.HTTP_400_BAD_REQUEST
+                {"error": "Wrong Credentials"}, status=status.HTTP_401_UNAUTHORIZED
             )
