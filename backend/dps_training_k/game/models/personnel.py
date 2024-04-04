c878f1fab7ec4af4309bd4b39e5f0399aa2902d2
Wolkenfarmer <jomaxw2@aol.com>
2024-04-04 16:48:34 +0200

    #82 backend: add foreign key to patient in personnel

--- backend/dps_training_k/game/models/personnel.py
+++ backend/dps_training_k/game/models/personnel.py
@@ -4,6 +4,9 @@ from django.db import models
 class Personnel(models.Model):
     name = models.CharField(max_length=100, blank=True)
     area = models.ForeignKey("Area", on_delete=models.CASCADE)
+    assigned_patient = models.ForeignKey(
+        "Patient", on_delete=models.SET_NULL, null=True, blank=True
+    )
 
     def __str__(self):
         return f"Personnel #{self.id} called {self.name} in area {self.area.name}"
