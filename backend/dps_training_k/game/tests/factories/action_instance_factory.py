e5d72b82852e7da26dce5adfc7389e31d60670cc
claasga <claas.gade@gmail.com>
2024-04-15 18:46:02 +0200

    #32 backend: add post generation hooks to factories to avoid circular dependencies

--- backend/dps_training_k/game/tests/factories/action_instance_factory.py
+++ backend/dps_training_k/game/tests/factories/action_instance_factory.py
@@ -20,24 +20,27 @@ class ActionInstanceStateFactory(factory.django.DjangoModelFactory):
     t_local_begin = 0
     t_local_end = None
 
-    @factory.post_generation
-    def set_current_state(self, create, extracted, **kwargs):
-        if not create:
-            return
-        # Update the ActionInstance `current_state` after creating ActionInstanceState
-        self.action_instance.current_state = self
-        self.action_instance.save()
-
 
 class ActionInstanceFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = ActionInstance
-        django_get_or_create = ("patient", "area", "action_template", "current_state")
+        django_get_or_create = (
+            "patient",
+            "area",
+            "action_template",
+            "current_state",
+        )
 
     patient = factory.SubFactory(PatientFactory)
     area = factory.SubFactory(AreaFactory)
     action_template = factory.SubFactory(ActionFactory)
     current_state = None
+
+    @factory.post_generation
+    def set_current_state(self, create, extracted, **kwargs):
+        if not create:
+            return
+        self.current_state = ActionInstanceStateFactory(action_instance=self)
 
 
 class FailedActionInstanceStateFactory(factory.django.DjangoModelFactory):
@@ -50,7 +53,7 @@ class FailedActionInstanceStateFactory(factory.django.DjangoModelFactory):
             "t_local_end",
         )
 
-    action_instance = factory.RelatedFactory(
+    action_instance = factory.SubFactory(
         "game.tests.factories.ActionInstanceFactoryFailedState"
     )
     name = ActionInstanceStateNames.DECLINED
@@ -67,6 +70,10 @@ class ActionInstanceFactoryFailedState(factory.django.DjangoModelFactory):
     patient = factory.SubFactory(PatientFactory)
     area = factory.SubFactory(AreaFactory)
     action_template = factory.SubFactory(ActionFactory)
-    current_state = factory.SubFactory(
-        "game.tests.factories.FailedActionInstanceStateFactory"
-    )
+    current_state = None
+
+    @factory.post_generation
+    def set_current_state(self, create, extracted, **kwargs):
+        if not create:
+            return
+        self.current_state = FailedActionInstanceStateFactory(action_instance=self)
