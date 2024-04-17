c2cbea42fcb3250add7f69410c4658fc10c8e1b1
Toni000 <tonipor27@gmail.com>
2024-04-17 16:45:27 +0200

    #89 backend: update conditions json and use "defaults" parameter

--- backend/dps_training_k/template/management/commands/minimal_actions.py
+++ backend/dps_training_k/template/management/commands/minimal_actions.py
@@ -14,27 +14,53 @@ class Command(BaseCommand):
         # Treatments
         Action.objects.update_or_create(
             name="i.V. Zugang",
-            category="TR",
-            application_duration=60,
-            effect_duration=120,  # depends on type of "Zugang"
-            conditions={"Personnel": 1, "Role": "Pflegefachkraft"},
+            defaults={
+                "category": "TR",
+                "application_duration": 60,
+                "effect_duration": 120,  # depends on type of "Zugang"
+                "conditions": {
+                    "required_actions": None,
+                    "prohibitive_actions": None,
+                    "material": {"i.V. Zugang": 1},
+                    "num_personnel": 1,
+                    "lab_devices": None,
+                    "area": None,
+                    "role": {"Pflegefachkraft": 1},
+                },
+            },
         )
         Action.objects.update_or_create(
             name="Vollelektrolyt 1000ml",
-            category="TR",
-            application_duration=0,  # depends on type of "Zugang"
-            effect_duration=120,
-            conditions={
-                "Personnel": 1,
-                "Role": "Pflegefachkraft",
-                "Actions_required": "i.V. Zugang",
+            defaults={
+                "category": "TR",
+                "application_duration": 0,
+                "effect_duration": 120,  # depends on type of "Zugang"
+                "conditions": {
+                    "required_actions": ["i.V. Zugang"],
+                    "prohibitive_actions": None,
+                    "material": None,
+                    "num_personnel": 1,
+                    "lab_devices": None,
+                    "area": None,
+                    "role": {"Pflegefachkraft": 1},
+                },
             },
         )
         # Examinations
         Action.objects.update_or_create(
             name="EKG anbringen",
-            category="EX",
-            application_duration=60,  # depends on type of "Zugang"
-            effect_duration=None,  # None means permanent
-            conditions={"Personnel": 1, "Role": "Pflegefachkraft"},
+            defaults={
+                "category": "EX",
+                "application_duration": 60,
+                "effect_duration": None,  # None means permanent
+                "conditions": {
+                    "required_actions": None,
+                    "prohibitive_actions": None,
+                    "material": None,
+                    "num_personnel": 1,
+                    "lab_devices": None,
+                    "area": None,
+                    "role": {"Pflegefachkraft": 1},
+                },
+            },
         )
