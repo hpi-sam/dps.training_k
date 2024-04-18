6852326446859e7ad4897dcdd75ca6ee4605e06b
claasga <claas.gade@gmail.com>
2024-04-18 17:23:32 +0200

    #127 backend: enforce patientStates having transitions

--- backend/dps_training_k/template/models/patient_state.py
+++ backend/dps_training_k/template/models/patient_state.py
@@ -6,9 +6,7 @@ class PatientState(models.Model):
         help_text="For Debugging purposes! State number as it is used in original data set",
         default=0,
     )
-    transition = models.ForeignKey(
-        "StateTransition", on_delete=models.CASCADE, null=True, blank=True
-    )
+    transition = models.ForeignKey("StateTransition", on_delete=models.CASCADE)
     data = models.JSONField(help_text="data for patient in current phase")
     state_depth = models.IntegerField()
     is_dead = models.BooleanField(default=False)
