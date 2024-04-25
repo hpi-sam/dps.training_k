dbb23a7b28b8f7e5d5ea0b1ee9818c449a3cc8ce
claasga <claas.gade@gmail.com>
2024-04-25 14:36:29 +0200

    #80 backend: make inventory smarter

--- backend/dps_training_k/game/models/inventory.py
+++ backend/dps_training_k/game/models/inventory.py
@@ -11,13 +11,14 @@ class InventoryEntry(models.Model):
         on_delete=models.CASCADE,
         related_name="entries",
     )
-    amount = models.IntegerField()
+    amount = models.IntegerField(default=0)
 
     def change(self, net_change):
-        if -net_change > self.amount:
-            raise ValueError(
-                "Not enough resources - might have happened because of a race condition"
-            )
+        # ToDo: Uncomment once condition checks are implemented
+        # if -net_change > self.amount:
+        #    raise ValueError(
+        #        "Not enough resources - might have happened because of a race condition"
+        #    )
         self.amount += net_change
         self.save(update_fields=["amount"])
         return self.amount
@@ -32,5 +33,5 @@ class Inventory(models.Model):
         return self.entries.filter(resource=resource).count()
 
     def change_resource(self, resource, net_change):
-        entry = self.entries.filter(resource=resource).first()
+        entry = InventoryEntry.objects.get_or_create(inventory=self, resource=resource)
         return entry.change(net_change)
