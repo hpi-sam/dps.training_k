686a1d6e0e3f56e9738fa18bc92459cf854323eb
JoshuaRiewesell <40123349+JoshuaRiewesell@users.noreply.github.com>
2024-04-26 11:55:48 +0200

    #160 frontend: add ButtonPopup

--- frontend/src/components/widgets/ExerciseControlPanel.vue
+++ frontend/src/components/widgets/ExerciseControlPanel.vue
@@ -3,6 +3,7 @@
 	import { ref, computed, watch } from "vue"
 	import socketTrainer from "@/sockets/SocketTrainer"
 	import { useExerciseStore } from "@/stores/Exercise"
+	import ButtonPopup from "./ButtonPopup.vue"
 
 	const exerciseStore = useExerciseStore()
 
@@ -11,6 +12,14 @@
 
 	status.value = exerciseStore.status
 	speed.value = exerciseStore.speed
+
+	watch(() => exerciseStore.status, () => {
+		status.value = exerciseStore.status
+	})
+
+	watch(() => exerciseStore.speed, () => {
+		speed.value = exerciseStore.speed
+	})
 
 	const info = computed(() => {
 		switch (status.value) {
@@ -27,11 +36,12 @@
 		}
 	})
 
-	function runExercise() {
-		if (status.value == "not-started")
-			socketTrainer.startExercise()
-		else if (status.value == "paused")
-			socketTrainer.resumeExercise()
+	function startExercise() {
+		socketTrainer.startExercise()
+	}
+
+	function resumeExercise() {
+		socketTrainer.resumeExercise()
 	}
 
 	function pauseExercise() {
@@ -41,8 +51,49 @@
 	function endExercise() {
 		socketTrainer.endExercise()
 	}
+
+	const showStartPopup = ref(false)
+	const showPausePopup = ref(false)
+	const showResumePopup = ref(false)
+	const showEndPopup = ref(false)
 </script>
 <template>
+	<ButtonPopup
+		v-if="showStartPopup"
+		title="Möchten Sie die Übung starten?"
+		button-text="Starten"
+		button-color="var(--green)"
+		button-text-color="white"
+		@button-click="startExercise"
+		@close-popup="showStartPopup = false"
+	/>
+	<ButtonPopup
+		v-if="showPausePopup"
+		title="Möchten Sie die Übung pausieren?"
+		button-text="Pausieren"
+		button-color="var(--yellow)"
+		button-text-color="white"
+		@button-click="pauseExercise"
+		@close-popup="showPausePopup = false"
+	/>
+	<ButtonPopup
+		v-if="showResumePopup"
+		title="Möchten Sie die Übung fortsetzen?"
+		button-text="Fortsetzen"
+		button-color="var(--green)"
+		button-text-color="white"
+		@button-click="resumeExercise"
+		@close-popup="showResumePopup = false"
+	/>
+	<ButtonPopup
+		v-if="showEndPopup"
+		title="Möchten Sie die Übung beenden?"
+		button-text="Beenden"
+		button-color="var(--red)"
+		button-text-color="white"
+		@button-click="endExercise"
+		@close-popup="showEndPopup = false"
+	/>
 	<div class="panel">
 		<div class="listItemButton">
 			<div class="listItemName">
@@ -50,10 +101,14 @@
 			</div>
 		</div>
 		<div class="rightButtons">
-			<button v-if="status != 'ended'" class="speedButton" @click="runExercise">
+			<button v-if="status != 'ended'" class="speedButton">
 				{{ speed }}x
 			</button>
-			<button v-if="status == 'not-started' || status == 'paused'" class="playButton" @click="runExercise">
+			<button
+				v-if="status == 'not-started'"
+				class="startButton"
+				@click="showStartPopup = true"
+			>
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
 					height="24"
@@ -64,7 +119,26 @@
 					<path :d="svg.playIcon" />
 				</svg>
 			</button>
-			<button v-if="status == 'running'" class="pauseButton" @click="pauseExercise">
+			<button
+				v-if="status == 'paused'"
+				class="startButton"
+				@click="showResumePopup = true"
+			>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					height="24"
+					viewBox="0 -960 960 960"
+					width="24"
+					fill="white"
+				>
+					<path :d="svg.playIcon" />
+				</svg>
+			</button>
+			<button
+				v-if="status == 'running'"
+				class="pauseButton"
+				@click="showPausePopup = true"
+			>
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
 					height="24"
@@ -75,7 +149,11 @@
 					<path :d="svg.pauseIcon" />
 				</svg>
 			</button>
-			<button v-if="status == 'running' || status == 'paused'" class="endButton" @click="endExercise">
+			<button
+				v-if="status == 'running' || status == 'paused'"
+				class="endButton"
+				@click="showEndPopup = true"
+			>
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
 					height="24"
@@ -110,13 +188,18 @@
 		display: flex;
 	}
 
-	.speedButton, .playButton, .pauseButton, .endButton {
+	.speedButton, .startButton, .pauseButton, .endButton {
 		height: 58px;
 		width: 60px;
 		border: none;
 	}
 
-	.playButton {
+	.speedButton {
+		font-size: 1.25rem;
+		line-height: 1.25rem;
+	}
+
+	.startButton {
 		background-color: var(--green);
 	}
 
