e76c20974c0479a74abd0c7d949567180fa19b70
claasga <claas.gade@gmail.com>
2024-04-28 21:40:51 +0200

    #80 backend: remove unneeded mixin for serializers

--- backend/dps_training_k/helpers/serialize_jsonable.py
+++
@@ -1,7 +0,0 @@
-from django.core.serializers.json import DjangoJSONEncoder
-import json
-
-
-class SerializeJSONAble:
-    def to_json(self):
-        return json.dumps(self, cls=DjangoJSONEncoder)
--- backend/dps_training_k/template/serializers/resource_serializer.py
+++ backend/dps_training_k/template/serializers/resource_serializer.py
@@ -1,9 +1,8 @@
 from rest_framework import serializers
 from template.models import Resource
-from helpers.serialize_jsonable import SerializeJSONAble
 
 
-class ResourceSerializer(SerializeJSONAble, serializers.ModelSerializer):
+class ResourceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Resource
         fields = ["name", "category", "is_returnable"]
