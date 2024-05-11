f48bcdbf78a761fb36b9993c8b2ca9784ad3c8fb
Wolkenfarmer <jomaxw2@aol.com>
2024-05-11 19:45:13 +0200

    frontend: add demo buttons to patient

--- frontend/src/components/screensPatient/ScreenInactive.vue
+++ frontend/src/components/screensPatient/ScreenInactive.vue
@@ -1,7 +1,22 @@
 <script setup lang="ts">
 	import {usePatientStore} from "@/stores/Patient"
+	import socketPatient, {serverMockEvents as serverMockEventsPatient} from "@/sockets/SocketPatient"
 
 	const patientStore = usePatientStore()
+
+	function startExercise() {
+		socketPatient.socket.onmessage({data: serverMockEventsPatient.find(e => e.id === 'exercise-started').data} as MessageEvent)
+	}
+
+	async function startDemo() {
+		socketPatient.socket.onmessage({data: serverMockEventsPatient.find(e => e.id === 'exercise').data} as MessageEvent)
+		socketPatient.socket.onmessage({data: serverMockEventsPatient.find(e => e.id === 'available-actions').data} as MessageEvent)
+		socketPatient.socket.onmessage({data: serverMockEventsPatient.find(e => e.id === 'exercise-started').data} as MessageEvent)
+		socketPatient.socket.onmessage({data: serverMockEventsPatient.find(e => e.id === 'state').data} as MessageEvent)
+		socketPatient.socket.onmessage({data: serverMockEventsPatient.find(e => e.id === 'ressource-assignments').data} as MessageEvent)
+		await new Promise(f => setTimeout(f, 100)) // bc visible-injuries is a bitch <3
+		socketPatient.socket.onmessage({data: serverMockEventsPatient.find(e => e.id === 'visible-injuries').data} as MessageEvent)
+	}
 </script>
 
 <template>
@@ -14,6 +29,23 @@
 				<div>Patient: {{ patientStore.patientName }} | {{ patientStore.patientId }}</div>
 				<div>Bereich: {{ patientStore.areaName }}</div>
 			</div>
+
+
+
+			<button
+				id="button"
+				:style="{backgroundColor: 'var(--green)', color: 'white', marginRight: '10px'}"
+				@click="startExercise"
+			>
+				Übung starten
+			</button>
+			<button
+				id="button"
+				:style="{backgroundColor: 'var(--yellow)', color: 'white'}"
+				@click="startDemo"
+			>
+				Demo starten
+			</button>
 		</div>
 	</div>
 </template>
@@ -40,5 +72,16 @@
 	.inactive-patient-details {
 		margin-bottom: 16px;
 		font-size: 1.25em;
+	}
+
+	#button {
+		position: relative;
+		border: 1px solid rgb(209, 213, 219);
+		border-radius: .5rem;
+		width: 180px;
+		font-size: 1.25rem;
+		padding: .75rem 1rem;
+		text-align: center;
+		margin-top: 10px;
 	}
 </style>
