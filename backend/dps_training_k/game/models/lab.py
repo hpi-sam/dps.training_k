34dc987cf4d77149dd8caefac5c7218c77dcd924
claasga <claas.gade@gmail.com>
2024-05-23 18:33:59 +0200

    #backend: send error message after trying performing action on dead patient, add test

--- backend/dps_training_k/game/models/lab.py
+++ backend/dps_training_k/game/models/lab.py
@@ -23,5 +23,11 @@ class Lab(models.Model):
     def personnel_available(self):
         return []
 
+    def can_receive_actions(self):
+        return True
+
+    def frontend_name(self):
+        return "Labor"
+
     def __str__(self):
         return f"Lab: {self.exercise.frontend_id}"
