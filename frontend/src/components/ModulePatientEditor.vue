4d7eee148f71a74a9bdc12a00c6e1b0de7af76bb
JoshuaRiewesell <40123349+JoshuaRiewesell@users.noreply.github.com>
2024-05-23 14:39:56 +0200

    add rete

--- frontend/src/components/ModulePatientEditor.vue
+++ frontend/src/components/ModulePatientEditor.vue
@@ -1,6 +1,25 @@
-<script setup lang="ts">
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { createEditor } from '@/rete'
+
+export default defineComponent({
+  async mounted() {
+    await createEditor(this.$refs.rete as HTMLElement)
+  }
+})
 </script>
 
 <template>
-	<h1>ModulePatientEditor</h1>
+	<main ref="rete" class="rete" />
 </template>
+
+<style scoped>
+.rete {
+  position: relative;
+  height: 100vh;
+  font-size: 1rem;
+  background: white;
+  text-align: left;
+  line-height: 1;
+}
+</style>
