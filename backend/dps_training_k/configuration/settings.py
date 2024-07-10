5e670aa4893020f08783334e6984bf46c9dbccdd
Toni000 <tonipor27@gmail.com>
2024-07-10 16:06:30 +0200

    #267 backend: fix CORS error

--- backend/dps_training_k/configuration/settings.py
+++ backend/dps_training_k/configuration/settings.py
@@ -32,7 +32,7 @@ Env.read_env(os.path.join(BASE_DIR, ".env.dev"))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG")
 CORS_ORIGIN_ALLOW_ALL = DEBUG
-CORS_ALLOWED_ORIGINS = [env.str("FRONTEND_URL")]
+CORS_ALLOWED_ORIGINS = ["https://klinik-dps.de", "https://www.klinik-dps.de", "http://localhost"]
 RUN_CONFIG = env.str("RUN_CONFIG", default="dev")
 
 CSRF_TRUSTED_ORIGINS = ["http://localhost:8000"]
