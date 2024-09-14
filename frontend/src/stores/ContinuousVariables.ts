8c45c0ac3074ea9d97bf7d1d5b0fe7cceeddbead
Wolkenfarmer <jomaxw2@aol.com>
2024-09-14 13:52:37 +0200

    #330 frontend: add continuous variables store with function update logic

---
+++ frontend/src/stores/ContinuousVariables.ts
@@ -0,0 +1,90 @@
+import {defineStore} from 'pinia'
+import {useExerciseStore} from "@/stores/Exercise"
+import {watch} from "vue"
+
+let intervalId: number | null = null
+
+export const useContinuousVariablesStore = defineStore('patientContinuous', {
+	state: (): ContinuousState => ({
+		timeUntilPhaseChange: Number.NEGATIVE_INFINITY,
+		continuousVariables: [],
+	}),
+	getters: {
+		getCurrentValueByName: (state) => {
+			return (variableName: string) => {
+				const variable = state.continuousVariables.find(v => v.name === variableName)
+				return variable ? variable.current : null
+			}
+		}
+	},
+	actions: {
+		loadContinuousVariablesFromJSON(continuousState: ContinuousState) {
+			this.timeUntilPhaseChange = continuousState.timeUntilPhaseChange
+
+			continuousState.continuousVariables.forEach(variable => {
+				const existingVariable = this.continuousVariables.find(v => v.name === variable.name)
+				if (existingVariable) {
+					existingVariable.target = variable.target
+					existingVariable.function = variable.function
+				} else this.continuousVariables.push(variable)
+			})
+
+			if (useExerciseStore().status == "running") startContinuousLogic()
+		},
+
+		startUpdatingContinuousVariables() {
+			if (intervalId !== null) return
+
+			intervalId = setInterval(() => {
+				if (this.timeUntilPhaseChange <= 0) {
+					this.stopUpdatingContinuousVariables()
+					return
+				}
+				this.continuousVariables.forEach(variable => {
+					variable.current = this.calculateVariableIncrement(variable)
+				})
+
+				this.timeUntilPhaseChange--
+			}, 1000)
+		},
+
+		calculateVariableIncrement(variable: ContinuousVariable) {
+			switch (variable.function) {
+				case "linear":
+					return variable.current + ((variable.target - variable.current) / this.timeUntilPhaseChange)
+				case "increment":
+					return variable.current + 1
+				case "decrement":
+					return variable.current - 1
+				default:
+					console.error("Unrecognized function: " + variable.function)
+					return variable.current
+			}
+		},
+
+		stopUpdatingContinuousVariables() {
+			this.timeUntilPhaseChange = 0
+			if (intervalId !== null) {
+				clearInterval(intervalId)
+				intervalId = null
+			}
+		}
+	}
+})
+
+export function startContinuousLogic() {
+	const patientStore = useContinuousVariablesStore()
+	const exerciseStore = useExerciseStore()
+
+	watch(
+		() => exerciseStore.status,
+		(newVal) => {
+			if (newVal === "running") {
+				patientStore.startUpdatingContinuousVariables()
+			} else {
+				patientStore.stopUpdatingContinuousVariables()
+			}
+		},
+		{immediate: true}
+	)
+}
