3df8bec8f7159220cc4ecb62b6008917c645d8bc
Wolkenfarmer <jomaxw2@aol.com>
2024-10-14 14:19:38 +0200

    #335 test: remove mock server events

--- testing/backend-stress/src/sockets/SocketTrainer.js
+++ testing/backend-stress/src/sockets/SocketTrainer.js
@@ -1,5 +1,4 @@
 import { WebSocket } from 'ws'; // Use require for WebSocket
-import { commonMockEvents } from './commonMockEvents.js'; // Use require for commonMockEvents
 
 class EventCallbacks {
 	constructor() {
@@ -210,61 +209,3 @@ export class SocketTrainer {
 		}));
 	}
 }
-
-const serverMockEvents = [
-	{
-		id: "available-material",
-		data: '{"messageType":"available-materials","availableMaterials":[' +
-			'{"materialName":"Beatmungsgerät","materialType":"DE"},' +
-			'{"materialName":"Blutdruckmessgerät","materialType":"DE"},' +
-			'{"materialName":"Defibrillator","materialType":"DE"},' +
-			'{"materialName":"Endoskop","materialType":"DE"},' +
-			'{"materialName":"Herz-Lungen-Maschine","materialType":"DE"},' +
-			'{"materialName":"Blut 0 negativ","materialType":"BL"},' +
-			'{"materialName":"Blut 0 positiv","materialType":"BL"},' +
-			'{"materialName":"Blut A negativ","materialType":"BL"},' +
-			'{"materialName":"Blut A positiv","materialType":"BL"},' +
-			'{"materialName":"Blut B negativ","materialType":"BL"},' +
-			'{"materialName":"Blut B positiv","materialType":"BL"},' +
-			'{"materialName":"Blut AB negativ","materialType":"BL"},' +
-			'{"materialName":"Blut AB positiv","materialType":"BL"}' +
-			']}'
-	},
-	{
-		id: 'log-update-1',
-		data: '{"messageType":"log-update","logEntries":[' +
-			'{"logId":"0","logMessage":"Patient wurde ins Krankenhaus eingeliefert. ' +
-			'Der Patient befindet sich in einem kritischen Zustand und benötigt sofortige Aufmerksamkeit.",' +
-			'"logTime":' + Date.UTC(2024, 2, 20, 14, 32, 20, 0) +
-			',"areaId":2,"patientId":"123456","personnelIds":[]},' +
-			'{"logId":"1","logMessage":"Behandlung des Patienten begonnen. ' +
-			'Dem Patienten werden die notwendigen Medikamente verabreicht und er steht unter ständiger Überwachung.",' +
-			'"logTime":' + Date.UTC(2024, 2, 20, 14, 31, 46, 0) +
-			',"areaId":1,"patientId":"123456","personnelIds":[11, 12]},' +
-			'{"logId":"2","logMessage":"Patient nach der Erstbehandlung stabilisiert. ' +
-			'Der Patient reagiert nun gut auf die Behandlung und wird beobachtet.",' +
-			'"logTime":' + Date.UTC(2024, 2, 20, 14, 33, 8, 0) +
-			',"areaId":2,"patientId":"754262","personnelIds":[13]}]}'
-	},
-	{
-		id: 'log-update-2',
-		data: '{"messageType":"log-update","logEntries":[' +
-			'{"logId":"4","logMessage":"Patient zur Zentralen Notaufnahme transportiert. ' +
-			'Der Patient wird für weitere Tests und Behandlungen verlegt.",' +
-			'"logTime":' + Date.UTC(2024, 2, 20, 14, 31, 10, 0) +
-			',"areaId":1,"patientId":"623422","personnelIds":[14]},' +
-			'{"logId":"5","logMessage":"Behandlung aufgrund unvorhergesehener Komplikationen abgebrochen. ' +
-			'Der Patient wird auf einen alternativen Behandlungsplan vorbereitet.",' +
-			'"logTime":' + Date.UTC(2024, 2, 20, 14, 33, 8, 0) +
-			',"areaId":2,"patientId":"123456","personnelIds":[]},' +
-			'{"logId":"6","logMessage":"Blut für den Patienten angefordert. ' +
-			'Der Patient benötigt eine Bluttransfusion, um seinen Zustand zu stabilisieren.",' +
-			'"logTime":' + Date.UTC(2024, 2, 20, 14, 32, 8, 0) +
-			',"areaId":1,"patientId":"623422","personnelIds":[15, 11]}]}'
-	},
-	{
-		id: 'set-speed',
-		data: '{"messageType":"set-speed","speed":2.5}'
-	},
-	...commonMockEvents
-];
